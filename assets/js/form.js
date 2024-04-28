a8f8d1a998da21283ada96331cfb099b84a930a0
faizahaque90 <163949218+faizahaque90@users.noreply.github.com>
2024-04-28 01:39:09 -0700

    erase some stuff for local storage

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,3 +1,39 @@
+// Selecting the necessary HTML elements for interaction
+const usernameInput = document.querySelector('#username');
+const titleInput = document.querySelector('#title');
+const contentInput = document.querySelector('#content');
+const submitButton = document.querySelector('#submit');
+let array = [];
+
+  
+  function renderLastRegistered() {
+    const email = localStorage.getItem('username');
+    const title = localStorage.getItem('title');
+    if (!usernameInput || !titleInput) {
+      return;
+    }
+    usernameInput.textContent = usernameInput;
+    titleInput.textContent = titleInput;
+  }
+  submitButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    const usernameInput = usernameInput.value;
+    const titleInput = titleInputInput.value;
+    if (email === '') {
+      displayMessage('error', 'Username cannot be blank');
+    } else if (password === '') {
+      displayMessage('error', 'Password cannot be blank');
+    } else {
+      displayMessage('success', 'Registered successfully');
+      localStorage.setItem('usernameInput', usernameInput);
+      localStorage.setItem('titleInput', titleInput);
+      renderLastRegistered();
+    }
+  });
+
+
+
+
 let darkMode = localStorage.getItem("darkMode");
 const darkModeToggle = document.querySelector("#dark-mode-toggle");
 
